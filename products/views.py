1982f682c8aaa3ac79fc2c35bd8105dca6bcd06b
Abdikafi Adan <Abdikafi.1adan@gmail.com>
2020-10-28 23:36:47 +0000

    added sorting by chossen category and fixat category link margin

--- products/views.py
+++ products/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 # Create your views here.
+
+
+def all_products(request):
+    """shows all products in the database and including search queries """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did't not enterany value search")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'active_categories': categories,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_info(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
